8db7d2c8fcb9d743445eb2a181069ba57e16b044
AlfikhoAzka <alfikhoazka33@gmail.com>
2024-07-30 19:09:23 +0700

    new-background

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,9 +5,9 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-12 bg-gradient-to-r from-indigo-600 to-pink-500">
         <div class="py-0 max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-x-auto shadow-md sm:rounded-lg">
+            <div class="bg-gradient-to-r from-indigo-600 to-pink-500 overflow-x-auto shadow-md sm:rounded-lg">
                 <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
@@ -19,7 +19,7 @@
                     </thead>
                     <tbody>
                         @foreach ($expenses as $expense)
-                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <tr class="bbg-white border-b dark:bg-gray-800 dark:border-gray-700">
                             <td scope="row"class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{$expense ->name}}
                             </td>
                             <td scope="row"class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{$expense ->price}}
