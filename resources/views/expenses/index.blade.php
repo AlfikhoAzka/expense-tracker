2a237cab57c61645413ad5985ae02d20094d8067
AlfikhoAzka <alfikhoazka33@gmail.com>
2024-08-01 21:04:19 +0700

    added-search-button-wip

--- resources/views/expenses/index.blade.php
+++ resources/views/expenses/index.blade.php
@@ -4,9 +4,20 @@
             {{ __('Expense') }}
         </h2>
     </x-slot>
-
-    <div class="py-0 max-w-7xl mx-auto sm:px-6 lg:px-8">    
-        <a href="{{ route ('expenses.create') }}" class="bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl text-white shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-bold py-2 px-4 rounded">Add Expense</a>
+    
+    <div class="py-0 max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="mx-auto max-w-screen-md sm:text-left">
+            <form action="#">
+                <div class="items-center mx-auto mb-3 space-y-4 max-w-screen-sm sm:flex sm:space-y-0">
+                    <div class="relative w-full">
+                        <input class="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search..." type="name" id="name">
+                    </div>
+                    <div>
+                        <button type="submit" class="py-3 px-5 w-full text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-primary-700 border-primary-600 sm:rounded-none sm:rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Search</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
 
     <div class="py-12 bg-gradient-to-r from-indigo-600 to-pink-500">
