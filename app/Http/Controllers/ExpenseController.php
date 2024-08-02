10fe012d9172975aec992ce8b162ff4d9a90e7d2
AlfikhoAzka <alfikhoazka33@gmail.com>
2024-08-02 14:42:00 +0700

    wip-controller

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -1,29 +1,31 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use App\Models\Expense;
 use Illuminate\Support\Facades\Log;
 
 class ExpenseController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $expenses = Expense::orderBy('created_at', 'desc')
-        ->paginate(10);
+        $expenses = Expense::paginate(10);
         return view ('expenses.index', compact('expenses'));
     }
 
     public function search(Request $request)
     {
-        $search = $request->input('search');
-        $expenses = Expense::where('name', 'like', '%' . $search . '%')
-        ->orderBy('created_at', 'desc')
-        ->paginate(10);
-        return view ('expenses.index', compact('expenses'));
+        $expenses = Expense::where('name', 1)
+                         ->orderBy('name')
+                         ->take(10)
+                         ->get();
+
+        return view('flights.index', compact('flights'));
     }
     public function create(expense $expense)
     {
-        return view ('expenses.create', compact('expense'));
+        return view('expenses.create', compact('expense'));
     }
     public function store(Request $request)
     {
@@ -32,13 +34,13 @@ class ExpenseController extends Controller
             'price' => 'required|numeric',
         ]);
         $expense = Expense::create(request()->all());
-        
+
         return redirect()->route('expenses.index')->with('success', 'Expense created successfully.');;
     }
 
     public function edit(Expense $expense)
     {
-        return view ('expenses.edit', compact('expense'));
+        return view('expenses.edit', compact('expense'));
     }
     public function update(Request $request, Expense $expense)
     {
@@ -47,7 +49,7 @@ class ExpenseController extends Controller
             'price' => 'required|numeric',
         ]);
         $expense->update($request->all());
-        
+
         return redirect()->route('expenses.index')->with('success', 'Expense updated successfully.');
     }
 
