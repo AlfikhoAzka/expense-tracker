4a663debf6a3de22423d8740330b12ce1bfe91e8
AlfikhoAzka <alfikhoazka33@gmail.com>
2024-08-07 19:56:13 +0700

    update-default

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -13,7 +13,7 @@ class ExpenseController extends Controller
         $search = $request->input('search');
         $start_date = $request->input('start_date');
         $end_date = $request->input('end_date');
-        $sortBy = $request->query('sort_by', 'name');
+        $sortBy = $request->query('sort_by', 'created_at');
         $sortOrder = $request->query('sort_order', 'asc');
 
         $expenses = Expense::search($search)
