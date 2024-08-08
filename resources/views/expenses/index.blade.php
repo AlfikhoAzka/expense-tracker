0fd65e268fbc90979ce49e9e71404c9ddd264d34
AlfikhoAzka <alfikhoazka33@gmail.com>
2024-08-08 14:32:04 +0700

    index-update

--- resources/views/expenses/index.blade.php
+++ resources/views/expenses/index.blade.php
@@ -62,6 +62,10 @@
                                 <a href="{{ request()->fullUrlWithQuery(['sort_by' => request()->query('sort_order') === 'price' ? '-price' : 'price']) }}">
                                 <div class="flex items-center">
                                     Price
+                                    @if (request('sort_by') === 'price' && request('sort_order') === 'asc')
+                                    @elseif (request('sort_by') === 'price' && request('sort_order') === 'desc')
+                                    @else
+                                    @endif
                                     <svg class="w-4 h-4 ms-1 ml-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
                                     </svg>
