c7c08e2c8e40f09a8e127b617aa51bf41fc14aba
AlfikhoAzka <alfikhoazka33@gmail.com>
2024-08-08 14:31:41 +0700

    index-update

--- resources/views/expenses/index.blade.php
+++ resources/views/expenses/index.blade.php
@@ -46,9 +46,13 @@
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
                             <th scope="col" class="px-6 py-3 w-2/4">
-                                <a href="{{ request()->fullUrlWithQuery(['sort_by' => request()->input('sort_order') === 'name-asc' ? '-name-desc' : 'name']) }}">
+                                <a href="{{ route('expenses.index', ['sort_by' => 'name', 'sort_order' => request('sort_order') === 'asc' ? 'desc' : 'asc']) }}">
                                 <div class="flex items-center">
                                     Name
+                                    @if (request('sort_by') === 'name' && request('sort_order') === 'asc')
+                                    @elseif (request('sort_by') === 'name' && request('sort_order') === 'desc')
+                                    @else
+                                    @endif
                                     <svg class="w-4 h-4 ms-1 ml-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
                                     </svg>
