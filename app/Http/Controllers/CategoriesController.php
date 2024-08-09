da8d6e7765853505ec810a56c3583679b7dd1fe0
AlfikhoAzka <alfikhoazka33@gmail.com>
2024-08-09 21:01:57 +0700

    update-categories-controller

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -27,4 +27,24 @@ class CategoriesController extends Controller
 
         return redirect()->route('categories.index')->with('success', 'Category added successfully.');
     }
+
+    public function edit(Categories $categories)
+    {
+        return view('categories.edit', compact('categories'));
+    }
+    public function update(Request $request, Categories $categories)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+        $categories->update($request->all());
+
+        return redirect()->route('categories.index')->with('success', 'Category updated successfully.');
+    }
+
+    public function destroy(Categories $categories)
+    {
+        $categories->delete();
+        return redirect()->route('categories.index')->with('success', 'Category deleted successfully.');
+    }
 }
