f08f85a739406aa92f455a51cde17c355635ac3e
AlfikhoAzka <alfikhoazka33@gmail.com>
2024-08-09 19:45:51 +0700

    add-create-blade

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -8,6 +8,23 @@
     <div class="py-12 bg-gradient-to-r from-indigo-600 to-pink-500">
         <div class="py-0 max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-x-auto shadow-md sm:rounded-lg mb-4">
+                <form method="POST" action="{{ route('categories.index') }}">
+                    @csrf
+                    
+                    <div class="p-6 bg-white dark:bg-gray-800 space-y-6">
+                        <div class="grid grid-cols-1">
+                            <div>
+                                <x-input-label for="name" :value="__('Name')" />
+                                <x-text-input id="name" name="name" type="text" class=" mt-1 block w-full" required autocomplete="name"/>
+                                <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                            </div>
+
+                            <div class="flex justify-between items-center mt-4">
+                                <a href="/categories" class="bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl text-white shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-bold py-2 px-4 rounded">Cancel</a>
+                            <button type="submit" class="bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl text-white shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-bold py-2 px-4 rounded">Add</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </div>
-        </div>
-    </div>
+</x-app-layout>
