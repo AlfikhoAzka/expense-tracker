d589bd1def61fa7c3096b8c229d9d31b859d7d8a
AlfikhoAzka <alfikhoazka33@gmail.com>
2024-08-09 18:13:31 +0700

    update-index-categories

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,7 +1,25 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Categories') }}
-        </h2>
-    </x-slot>
+    <div class="py-12 bg-gradient-to-r from-indigo-600 to-pink-500">
+        <div class="py-0 max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-x-auto shadow-md sm:rounded-lg mb-4">
+                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3 w-2/4">Category
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($categories as $category)
+                        <tr class="bbg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <td scope="row"class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{$category ->name}}
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            {{ $categories->links() }}
+        </div>
+    </div>
 </x-app-layout>
