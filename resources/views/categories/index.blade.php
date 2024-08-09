ae5314dfe1f03914705a5f180c1f024129487e85
AlfikhoAzka <alfikhoazka33@gmail.com>
2024-08-09 21:02:12 +0700

    update-index-categories

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,6 +1,8 @@
 <x-app-layout>
-    <div class="pt-10 max-w-7xl mx-auto sm:px-6 lg:px-8">    
-        <a href="categories/create" class="bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl text-white shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-bold py-2 px-4 rounded">Add Category</a>
+    <div class="pt-10 max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <a href="categories/create"
+            class="bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl text-white shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-bold py-2 px-4 rounded">Add
+            Category</a>
     </div>
     <div class="py-12 bg-gradient-to-r from-indigo-600 to-pink-500">
         <div class="py-0 max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -8,14 +10,30 @@
                 <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
-                            <th scope="col" class="px-6 py-3 w-2/4">Category
+                            <th scope="col" class="px-6 py-3 w-3/4">Category
+                            </th>
+                            <th scope="col" class="px-6 py-3 w-2/4">Actions
                             </th>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach ($categories as $category)
                         <tr class="bbg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <td scope="row"class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{$category ->name}}
+                            <td scope="row"
+                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{$category->name}}
+                            </td>
+                            <td class="px-6">
+                                <div class="flex space-x-2">
+                                    <a href="{{ route('categories.edit', $category->id ) }}"
+                                        class="bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl text-white shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-bold py-2 px-4 rounded">Edit</a>
+                                    <form action="{{ route('categories.destroy', $category->id ) }} " method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" onclick="return confirm('Are you sure?')"
+                                            class="bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl text-white shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-bold py-2 px-4 rounded">Delete</button>
+                                    </form>
+                                </div>
                             </td>
                         </tr>
                         @endforeach
