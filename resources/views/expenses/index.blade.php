5b4f5e003dc444d841778bc9a3e5711d5477145a
AlfikhoAzka <alfikhoazka33@gmail.com>
2024-08-09 10:27:57 +0700

    update-index-due-to-accessor

--- resources/views/expenses/index.blade.php
+++ resources/views/expenses/index.blade.php
@@ -1,38 +1,62 @@
 <x-app-layout>
     <div class="pt-10 max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="flex items-center space-x-4">
-            <a href="{{ route('expenses.create') }}" class="bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl text-white shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-bold py-2 px-4 rounded">Add Expense</a>
+            <a href="{{ route('expenses.create') }}"
+                class="bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl text-white shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-bold py-2 px-4 rounded">Add
+                Expense</a>
             <form action="/expenses" method="GET" class="flex-grow">
                 <div class="flex items-center space-x-4">
                     <div class="relative w-3/12">
-                        <label for="search" class="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Search</label>
+                        <label for="search"
+                            class="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Search</label>
                         <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
+                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
+                                    d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z" />
                             </svg>
                         </div>
-                        <input class="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search..." type="search" id="search" name="search" value="{{ request('search') }}">
+                        <input
+                            class="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="Search..." type="search" id="search" name="search"
+                            value="{{ request('search') }}">
                     </div>
                     <div class="relative w-3/12">
-                        <label for="Start date" class="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Start date</label>
+                        <label for="Start date"
+                            class="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Start date</label>
                         <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
+                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
+                                    d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z" />
                             </svg>
                         </div>
-                        <input class="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Start date..." type="datetime-local" id="start_date" name="start_date" value="{{ request('start_date') }}">
+                        <input
+                            class="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="Start date..." type="datetime-local" id="start_date" name="start_date"
+                            value="{{ request('start_date') }}">
                     </div>
                     <div class="relative w-3/12">
-                        <label for="End date" class="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">End date</label>
+                        <label for="End date"
+                            class="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">End date</label>
                         <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
+                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
+                                    d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z" />
                             </svg>
                         </div>
-                        <input class="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="End date..." type="datetime-local" id="end_date" name="end_date" value="{{ request('end_date') }}">
+                        <input
+                            class="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="End date..." type="datetime-local" id="end_date" name="end_date"
+                            value="{{ request('end_date') }}">
                     </div>
                     <div>
-                        <button type="submit" class="py-3 px-5 w-full text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-primary-700 border-primary-600 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Search</button>
+                        <button type="submit"
+                            class="py-3 px-5 w-full text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-primary-700 border-primary-600 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Search</button>
                     </div>
                 </div>
             </form>
@@ -46,45 +70,58 @@
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
                             <th scope="col" class="px-6 py-3 w-2/4">
-                                <a href="{{ route('expenses.index', ['sort_by' => 'name', 'sort_order' => request('sort_order') === 'asc' ? 'desc' : 'asc']) }}">
-                                <div class="flex items-center">
-                                    Name
-                                    @if (request('sort_by') === 'name' && request('sort_order') === 'asc')
-                                    @elseif (request('sort_by') === 'name' && request('sort_order') === 'desc')
-                                    @else
-                                    @endif
-                                    <svg class="w-4 h-4 ms-1 ml-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
-                                    </svg>
-                                </div>
-                                 </a>
+                                <a
+                                    href="{{ route('expenses.index', ['sort_by' => 'name', 'sort_order' => request('sort_order') === 'asc' ? 'desc' : 'asc']) }}">
+                                    <div class="flex items-center">
+                                        Name
+                                        @if (request('sort_by') === 'name' && request('sort_order') === 'asc')
+                                        @elseif (request('sort_by') === 'name' && request('sort_order') === 'desc')
+                                        @else
+                                        @endif
+                                        <svg class="w-4 h-4 ms-1 ml-1" aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                            viewBox="0 0 24 24">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4" />
+                                        </svg>
+                                    </div>
+                                </a>
                             </th>
                             <th scope="col" class="px-6 py-3 w-2/4">
-                                <a href="{{ route('expenses.index', ['sort_by' => 'price', 'sort_order' => request('sort_order') === 'asc' ? 'desc' : 'asc']) }}">
-                                <div class="flex items-center">
-                                    Price
-                                    @if (request('sort_by') === 'price' && request('sort_order') === 'asc')
-                                    @elseif (request('sort_by') === 'price' && request('sort_order') === 'desc')
-                                    @else
-                                    @endif
-                                    <svg class="w-4 h-4 ms-1 ml-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
-                                    </svg>
-                                </div>
+                                <a
+                                    href="{{ route('expenses.index', ['sort_by' => 'price', 'sort_order' => request('sort_order') === 'asc' ? 'desc' : 'asc']) }}">
+                                    <div class="flex items-center">
+                                        Price
+                                        @if (request('sort_by') === 'price' && request('sort_order') === 'asc')
+                                        @elseif (request('sort_by') === 'price' && request('sort_order') === 'desc')
+                                        @else
+                                        @endif
+                                        <svg class="w-4 h-4 ms-1 ml-1" aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                            viewBox="0 0 24 24">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4" />
+                                        </svg>
+                                    </div>
                                 </a>
                             </th>
                             <th scope="col" class="px-6 py-3">
-                                <a href="{{ route('expenses.index', ['sort_by' => 'created_at', 'sort_order' => request('sort_order') === 'asc' ? 'desc' : 'asc']) }}">
-                                <div class="flex items-center">
-                                    Created at
-                                    @if (request('sort_by') === 'created_at' && request('sort_order') === 'asc')
-                                    @elseif (request('sort_by') === 'created_at' && request('sort_order') === 'desc')
-                                    @else
-                                    @endif
-                                    <svg class="w-4 h-4 ms-1 ml-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
-                                    </svg>
-                                </div>
+                                <a
+                                    href="{{ route('expenses.index', ['sort_by' => 'created_at', 'sort_order' => request('sort_order') === 'asc' ? 'desc' : 'asc']) }}">
+                                    <div class="flex items-center">
+                                        Created at
+                                        @if (request('sort_by') === 'created_at' && request('sort_order') === 'asc')
+                                        @elseif (request('sort_by') === 'created_at' && request('sort_order') ===
+                                        'desc')
+                                        @else
+                                        @endif
+                                        <svg class="w-4 h-4 ms-1 ml-1" aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                            viewBox="0 0 24 24">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4" />
+                                        </svg>
+                                    </div>
                                 </a>
                             </th>
                             <th scope="col" class="px-6 py-3">
@@ -96,16 +133,16 @@
                         @foreach ($expenses as $expense)
                         <tr class="bbg-white border-b dark:bg-gray-800 dark:border-gray-700">
                             <td scope="row"
-                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{$expense
-                                ->name}}
+                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{$expense->name}}
                             </td>
                             <td scope="row"
                                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                {{$expense ->price}}
+                                {{$expense->price_formatted}}
                             </td>
                             <td scope="row"
-                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{$expense
-                                ->created_at}}
+                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{$expense->created_at}}
                             </td>
                             <td class="px-6">
                                 <div class="flex space-x-2">
