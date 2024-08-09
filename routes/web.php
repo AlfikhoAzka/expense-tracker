f9fc694a12ff2633a4fb2013eb4166254135eb2b
AlfikhoAzka <alfikhoazka33@gmail.com>
2024-08-09 12:34:16 +0700

    new-route

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::get('/dashboard', function () {
 
 Route::middleware('auth')->group(function () {
     Route::resource('expenses', ExpenseController::class);
+    Route::resource('category', ExpenseController::class);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
