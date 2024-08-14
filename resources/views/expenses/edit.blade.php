cd1e566a8b8edb494f9ce47c9b6dfc92cf9dc6fb
AlfikhoAzka <alfikhoazka33@gmail.com>
2024-08-14 09:21:58 +0700

    edt-blade-expenses

--- resources/views/expenses/edit.blade.php
+++ resources/views/expenses/edit.blade.php
@@ -16,17 +16,31 @@
                         <div class="grid grid-cols-1">
                             <div>
                                 <x-input-label for="name" :value="__('Name')" />
-                                <x-text-input id="name" name="name" type="text" class=" mt-1 block w-full" value="{{ $expense->name }}" required autocomplete="name"/>
+                                <x-text-input id="name" name="name" type="text" class=" mt-1 block w-full"
+                                    value="{{ $expense->name }}" required autocomplete="name" />
                                 <x-input-error class="mt-2" :messages="$errors->get('name')" />
-                            
+
+                                <x-input-label for="categories" :value="__('Choose Category')" />
+                                <select id="categories" name="category_id"
+                                    class="mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
+                                    required autocomplete="selection">
+                                    @foreach ($categories as $category)
+                                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                    @endforeach
+                                </select>
+                                <x-input-error class="mt-2" :messages="$errors->get('category_id')" />
+
                                 <x-input-label for="price" :value="__('Price')" />
-                                <x-text-input id="price" name="price" type="text" class="mt-1 block w-full" value="{{ $expense->price }}" required autocomplete="price"/>
+                                <x-text-input id="price" name="price" type="text" class="mt-1 block w-full"
+                                    value="{{ $expense->price }}" required autocomplete="price" />
                                 <x-input-error class="mt-2" :messages="$errors->get('price')" />
                             </div>
 
                             <div class="flex justify-between items-center mt-4">
-                                <a href="/expenses" class="bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl text-white shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-bold py-2 px-4 rounded">Cancel</a>
-                            <button type="submit" class="bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl text-white shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-bold py-2 px-4 rounded">Update</button>
+                                <a href="/expenses"
+                                    class="bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl text-white shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-bold py-2 px-4 rounded">Cancel</a>
+                                <button type="submit"
+                                    class="bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl text-white shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-bold py-2 px-4 rounded">Update</button>
                             </div>
                         </div>
                     </div>
@@ -35,4 +49,3 @@
         </div>
     </div>
 </x-app-layout>
-
