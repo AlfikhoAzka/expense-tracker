397aa4b354e971df3b1dada5bf21d38e50953419
AlfikhoAzka <alfikhoazka33@gmail.com>
2024-08-14 11:54:43 +0700

    showing-category_id

--- resources/views/expenses/index.blade.php
+++ resources/views/expenses/index.blade.php
@@ -156,7 +156,7 @@
                             </td>
                             <td scope="row"
                                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                {{$expense->categories}}
+                                {{$expense->category_id}}
                             </td>
                             <td scope="row"
                                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
