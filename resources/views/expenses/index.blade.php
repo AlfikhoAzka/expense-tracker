0fa7997a7e888341c3cf889b8c653eff964c0713
AlfikhoAzka <alfikhoazka33@gmail.com>
2024-08-15 09:31:49 +0700

    added-filtering-not-working-yet

--- resources/views/expenses/index.blade.php
+++ resources/views/expenses/index.blade.php
@@ -54,8 +54,24 @@
                             placeholder="End date..." type="datetime-local" id="end_date" name="end_date"
                             value="{{ request('end_date') }}">
                     </div>
-
-                    
+                    <div class="relative w-3/12">
+                        <label for="Category"
+                            class="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Category</label>
+                        <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
+                                    d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z" />
+                            </svg>
+                        </div>
+                        <select id="categories" class="form-control block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                            <option value="">Choose Category</option>
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}">{{ $category->category }}</option>
+                            @endforeach
+                        </select>
+                    </div>
                     <div>
                         <button type="submit"
                             class="py-3 px-5 w-full text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-primary-700 border-primary-600 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Search</button>
