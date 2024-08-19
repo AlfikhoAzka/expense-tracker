823bc1e26b7cdcf8f2d26250aea39809fcbb7a48
AlfikhoAzka <alfikhoazka33@gmail.com>
2024-08-19 11:58:12 +0700

    new-box-size

--- resources/views/expenses/create.blade.php
+++ resources/views/expenses/create.blade.php
@@ -8,18 +8,18 @@
     <div class="py-12 bg-gradient-to-r from-indigo-600 to-pink-500">
         <div class="py-0 max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-x-auto shadow-md sm:rounded-lg mb-4">
-                <form method="POST" action="{{ route('expenses.index') }}">
+                <form method="POST" action="{{ route('expenses.index') }}" enctype="multipart/form-data">
                     @csrf
                     
                     <div class="p-6 bg-gray-800 dark:bg-gray-800 space-y-6">
                         <div class="grid grid-cols-1">
                             <div>
-                                <x-input-label for="name" :value="__('Name')" />
-                                <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" required autocomplete="name"/>
+                                <x-input-label for="name" :value="__('Name')"/>
+                                <x-text-input id="name" name="name" type="text" class="size-10 mt-1 block w-full" required autocomplete="name"/>
                                 <x-input-error class="mt-2" :messages="$errors->get('name')" />
 
-                                <x-input-label for="category" :value="__('Choose Category')" />
-                                <select id="categories" name="category_id" class="mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" autocomplete="selection">
+                                <x-input-label for="category" :value="__('Choose Category')" class="mt-3" />
+                                <select id="categories" name="category_id" class="size-10 mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" autocomplete="selection">
                                         <option value="">Choose Category</option>
                                     @foreach ($categories as $category)
                                         <option value="{{ $category->id }}">{{ $category->category }}</option>
@@ -27,8 +27,12 @@
                                 </select>
                                 <x-input-error class="mt-2" :messages="$errors->get('category_id')" />
                             
-                                <x-input-label for="price" :value="__('Price')" />
-                                <x-text-input id="price" name="price" type="text" class="mt-1 block w-full" required autocomplete="price"/>
+                                <x-input-label for="price" :value="__('Price')" class="mt-3" />
+                                <x-text-input id="price" name="price" type="text" class="size-10 mt-1 block w-full" required autocomplete="price"/>
+                                <x-input-error class="mt-2" :messages="$errors->get('price')" />
+
+                                <x-input-label for="image" :value="__('Add Image')" class="mt-3" />
+                                <x-text-input id="image" name="image" type="file" class="size-10 mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" />
                                 <x-input-error class="mt-2" :messages="$errors->get('price')" />
                             </div>
 
