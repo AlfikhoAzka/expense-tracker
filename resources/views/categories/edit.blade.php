1ca86b608969ee0faf8aea599625ab25168943d2
AlfikhoAzka <alfikhoazka33@gmail.com>
2024-08-21 15:13:15 +0700

    edit-blade-update

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -16,7 +16,7 @@
                         <div class="grid grid-cols-1">
                             <div>
                                 <x-input-label for="name" :value="__('Name')" />
-                                <x-text-input id="name" name="name" type="text" class=" mt-1 block w-full" value="{{ $category->name }}" required autocomplete="name"/>
+                                <x-text-input id="name" name="name" type="text" class=" mt-1 block w-full" value="{{ $category->category }}" required autocomplete="name"/>
                                 <x-input-error class="mt-2" :messages="$errors->get('name')" />
                             </div>
 
