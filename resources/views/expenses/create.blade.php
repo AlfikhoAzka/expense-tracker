ebb0670232ad08022b09e6ef90987c0062a2cd75
AlfikhoAzka <alfikhoazka33@gmail.com>
2024-08-22 16:48:46 +0700

    add-old()

--- resources/views/expenses/create.blade.php
+++ resources/views/expenses/create.blade.php
@@ -10,25 +10,28 @@
             <div class="bg-white dark:bg-gray-800 overflow-x-auto shadow-md sm:rounded-lg mb-4">
                 <form method="POST" action="{{ route('expenses.store') }}" enctype="multipart/form-data">
                     @csrf
-                    
+
                     <div class="p-6 bg-gray-800 dark:bg-gray-800 space-y-6">
                         <div class="grid grid-cols-1">
                             <div>
-                                <x-input-label for="name" :value="__('Name')"/>
-                                <x-text-input id="name" name="name" type="text" class="size-10 mt-1 block w-full" required autocomplete="name"/>
+                                <x-input-label for="name" :value="__('Name')" />
+                                <x-text-input id="name" name="name" type="text" class="size-10 mt-1 block w-full" required autocomplete="name" :value="old('name')" />
                                 <x-input-error class="mt-2" :messages="$errors->get('name')" />
 
                                 <x-input-label for="category" :value="__('Choose Category')" class="mt-3" />
-                                <select id="categories" name="category_id" class="size-10 mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" autocomplete="selection">
-                                        <option value="">Choose Category</option>
+                                <select id="categories" name="category_id"
+                                    class="size-10 mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" autocomplete="selection" :value="old('category_id')">
+                                    <option value="">Choose Category</option>
                                     @foreach ($categories as $category)
-                                        <option value="{{ $category->id }}">{{ $category->category }}</option>
+                                    <option value="{{ $category->id }}" {{ old('category_id')==$category->id ? 'selected' : '' }}>
+                                        {{ $category->category }}
+                                    </option>
                                     @endforeach
                                 </select>
                                 <x-input-error class="mt-2" :messages="$errors->get('category_id')" />
-                            
+
                                 <x-input-label for="price" :value="__('Price')" class="mt-3" />
-                                <x-text-input id="price" name="price" type="text" class="size-10 mt-1 block w-full" required autocomplete="price"/>
+                                <x-text-input id="price" name="price" type="text" class="size-10 mt-1 block w-full" required autocomplete="price" :value="old('price')" />
                                 <x-input-error class="mt-2" :messages="$errors->get('price')" />
 
                                 <x-input-label for="image" :value="__('Add Image')" class="mt-3" />
@@ -37,8 +40,10 @@
                             </div>
 
                             <div class="flex justify-between items-center mt-4">
-                                <a href="/expenses" class="bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl text-white shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-bold py-2 px-4 rounded">Cancel</a>
-                                <button type="submit" class="bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl text-white shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-bold py-2 px-4 rounded">Add</button>
+                                <a href="/expenses"
+                                    class="bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl text-white shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-bold py-2 px-4 rounded">Cancel</a>
+                                <button type="submit"
+                                    class="bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl text-white shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-bold py-2 px-4 rounded">Add</button>
                             </div>
                         </div>
                     </div>
